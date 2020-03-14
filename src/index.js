233f5f4bcc4e70713f4991eeca396cc669e2ec0f
Eric <eric22@inbox.ru>
2020-03-14 16:01:55 +0300

    добавлен класс UIController

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Test from "./classes/Test";
+import Service from "./classes/Service";
+import UIController from "./classes/UIController";
 
-window.addEventListener("load", () => {
-    window.test = new Test();
+const test = new Test({
+    service: new Service("./data.json"),
+    ui: new UIController(),
 });
